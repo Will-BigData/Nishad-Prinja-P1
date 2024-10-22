71e2879d49946edb031b87240275cf18072f9046
nishadprinja <np327@cornell.edu>
2024-10-22 14:31:17 -0400

    working version

--- pystore.py
+++ pystore.py
@@ -50,7 +50,7 @@ def main():
 
         find_admin = admin_collection.count_documents( { "$and": [ {"user" : {"$eq": adminUsername.lower()} }, {"password" : {"$eq" : adminPassword}} ] })
         log_collection.insert_one( { "log" : "Counted records of matching Admin/password!", "when": datetime.now() } )
-        print(find_admin)
+        # print(find_admin)
         if (find_admin):
             print("You have successfully logged in!")
             log_collection.insert_one( { "log" : "Admin logged in!", "when": datetime.now() } )
@@ -136,7 +136,7 @@ def main():
                 while(True):
                     customer_user = input("What customer would you like to edit? ")
                     if ((customer_collection.count_documents( { "user" : { "$eq": customer_user.lower() } } )) == 0):
-                        log_collection.insert_one( { "log" : "Queried database for existing user during UPDATE!", "when": datetime.now() } )
+                        log_collection.insert_one( { "log" : "Queried database for non-existing user during UPDATE!", "when": datetime.now() } )
                         print("The customer username you searched for does not exist!")
                         continue
                     customer_updated_user = input("\033[1m" + "What would you like to edit? The (u)sername or (p)assword? " + "\033[0m")
@@ -157,7 +157,7 @@ def main():
                 while(True):
                     customer_user = input("What customer would you like to delete? ")
                     if ((customer_collection.count_documents( { "user" : { "$eq": customer_user.lower() } } )) == 0):
-                        log_collection.insert_one( { "log" : "Queried database for existing user during DELETE!", "when": datetime.now() } )
+                        log_collection.insert_one( { "log" : "Queried database for non-existing user during DELETE!", "when": datetime.now() } )
                         print("The customer username you searched for does not exist!")
                         continue
                     customer_collection.delete_one( { "user" : customer_user.lower() } )
@@ -165,15 +165,21 @@ def main():
                     print(f"Customer username {customer_user} deleted!")
                     return
         
-        update_or_delete_store = input("\033[1m" + "Would you like to (e)dit or (d)elete items from the store? Or (n)ot? " + "\033[0m")
+        update_or_delete_store = input("\033[1m" + "Would you like to (a)dd, (e)dit, or (d)elete items from the store? Or (n)ot? " + "\033[0m")
         match (update_or_delete_store.strip().lower()[0]):
             case('n'):
                 pass
+            case('a'):
+                item_to_add = input("What is the name of the item you would like to add? ")
+                cost_of_item = input("What is the cost of the item you are adding? ")
+                deku_collection.insert_one( { "item" : item_to_add, "cost": int(cost_of_item) } )
+                log_collection.insert_one( { "log" : f"ADDed item to store {item_to_add} with cost {cost_of_item}!", "when": datetime.now() } )
+                print("\033[1m" + f"You added {item_to_add} to the Deku Store with a cost of {cost_of_item}!" + "\033[0m")
             case('e'):
                 while(True):
                     store_item_update = input("What item would you like to edit? ")
                     if ((deku_collection.count_documents( { "item" : { "$eq": store_item_update } } )) == 0):
-                        log_collection.insert_one( { "log" : "Queried database for existing STORE ITEM during UPDATE!", "when": datetime.now() } )
+                        log_collection.insert_one( { "log" : "Queried database for non-existing STORE ITEM during UPDATE!", "when": datetime.now() } )
                         print("The item you searched for does not exist!")
                         continue
                     deku_updated_item = input("\033[1m" + "What would you like to edit? The (i)tem or (c)ost? " + "\033[0m")
@@ -194,13 +200,22 @@ def main():
                 while(True):
                     store_item_delete = input("What item would you like to delete? ")
                     if ((deku_collection.count_documents( { "item" : { "$eq": store_item_delete } } )) == 0):
-                        log_collection.insert_one( { "log" : "Queried database for existing STORE ITEM during DELETE!", "when": datetime.now() } )
+                        log_collection.insert_one( { "log" : "Queried database for non-existing STORE ITEM during DELETE!", "when": datetime.now() } )
                         print("The item you searched for does not exist!")
                         continue
                     deku_collection.delete_one( { "item" : store_item_delete } )
                     log_collection.insert_one( { "log" : f"DELETEd item {store_item_delete}!", "when": datetime.now() } )
                     print(f"Item named {store_item_delete} deleted!")
                     return
+        output_logs = input("\033[1m" + "Would you like to (o)utput logs Or (n)ot? " + "\033[0m")
+        match (output_logs.strip().lower()[0]):
+            case('n'):
+                pass
+            case('o'):
+                all_logs = log_collection.find()
+                for log in all_logs:
+                    print(log["log"], " | recorded: " + str(log["when"]))
+
 
     if (customer_logged_in):
             while(True):
@@ -213,7 +228,7 @@ def main():
                                 case('b'):
                                     broken_pots_rupees = random.randint(5, 11)
                                     customer_collection.update_one( { "user" : customer_username.lower() }, { "$inc" : { "wallet" : broken_pots_rupees } } )
-                                    log_collection.insert_one( { "log" : f"Broke pots to increase wallet by {broken_pots_rupees}!", "when": datetime.now() } )
+                                    log_collection.insert_one( { "log" : f"UPDATEd after breaking pots to increase wallet by {broken_pots_rupees}!", "when": datetime.now() } )
                                     current_wallet = customer_collection.distinct("wallet", { "user" : customer_username.lower() } )
                                     log_collection.insert_one( { "log" : f"QEURIEd for current amount in wallet after BREAKING POTS!", "when": datetime.now() } )
                                     print("You broke pots to collect rupees!")
@@ -223,7 +238,7 @@ def main():
                                 case('c'):
                                     cut_grass = random.randint(1, 14)
                                     customer_collection.update_one( { "user" : customer_username.lower() }, { "$inc" : { "wallet" : cut_grass } } )
-                                    log_collection.insert_one( { "log" : f"Cut grass to increase wallet by {cut_grass}!", "when": datetime.now() } )
+                                    log_collection.insert_one( { "log" : f"UPDATEd after cutting grass to increase wallet by {cut_grass}!", "when": datetime.now() } )
                                     current_wallet = customer_collection.distinct("wallet", { "user" : customer_username.lower() } )
                                     log_collection.insert_one( { "log" : f"QEURIEd for current amount in wallet after CUTTING GRASS!", "when": datetime.now() } )
                                     print("You cut a patch of tall grass and gathered the rupees that fell out!")
@@ -233,7 +248,7 @@ def main():
                                 case('k'):
                                     kill_enemy = random.randint(3, 16)
                                     customer_collection.update_one( { "user" : customer_username.lower() }, { "$inc" : { "wallet" : kill_enemy } } )
-                                    log_collection.insert_one( { "log" : f"Cut grass to increase wallet by {kill_enemy}!", "when": datetime.now() } )
+                                    log_collection.insert_one( { "log" : f"UPDATEd after killing enemy to increase wallet by {kill_enemy}!", "when": datetime.now() } )
                                     current_wallet = customer_collection.distinct("wallet", { "user" : customer_username.lower() } )
                                     log_collection.insert_one( { "log" : f"QEURIEd for current amount in wallet after KILLING ENEMY!", "when": datetime.now() } )
                                     enemy_type = ["Amy", "Anubis", "Armos", "Baby Dodongo", "Bari", "Beamos", "Beth", "Big Blade", "Big Deku Baba", "Big Poe", "Big Skulltula", "Biri", "Blade Trap", "Blue Bubble", "Blue Tektite", "Boulder", "Business Scrub", "Club Moblin", "Deku Baba", "Deku Scrub", "Dinolfos", "Dodongo", "Trap Door", "Fire Eye", "Fire Keese", "Flame Wall", "Flat", "Floor Spikes", "Floormaster", "Flying Pot", "Crazy Floor Tile", "Freezard", "Gerudo Guard", "Gibdo", "Gohma Larva", "Gold Skulltula", "Green Bubble", "Guay", "Guillotine", "Ice Keese", "Ice Scythe", "Joelle", "Keese", "Leever", "Like Like", "Lizalfos", "Mad Scrub", "Magma Bomb", "Meg", "Moblin", "Octorok", "Parasitic Tentacle", "Peahat", "Peahat Larva", "Poe", "ReDead", "Red Bubble", "Red Tektite", "Shabom", "Sharp", "Shell Blade", "Skulltula", "Skullwalltula", "Skull Kid", "Spike", "Spiked-Log Trap", "Stalchild", "Stalfos", "Stinger", "Tailpasaran", "Torch Slug", "Toxic Water", "Wallmaster", "White Bubble", "White Wolfos", "Wolfos"]
@@ -249,9 +264,10 @@ def main():
                     case('b'):
                         while(True):
                             entire_store = deku_collection.find()
+                            entire_inventory = inventory_collection.find()
                             log_collection.insert_one( { "log" : f"QEURIEd for ENTIRE STORE!", "when": datetime.now() } )
                             # print(entire_store)
-                            store_decision = input("Would you like to (b)uy or (s)ell? Or (e)xit? ")
+                            store_decision = input("\033[1m" + "Would you like to (b)uy or (s)ell? Or (e)xit? " + "\033[0m")
                             match(store_decision.strip().lower()[0]):
                                 case('b'):
                                     # pass
@@ -259,13 +275,13 @@ def main():
                                     for item in entire_store:
                                         print(item["item"], " | cost: " + str(item["cost"]))
                                     current_wallet = customer_collection.distinct("wallet", { "user" : customer_username.lower() } )
+                                    log_collection.insert_one( { "log" : f"QEURIEd for current amount in wallet during TRANSACTION!", "when": datetime.now() } )
                                     print(f"Your wallet has {current_wallet[0]} rupees!")
                                     item_bought = input("What item would you like to buy? ")
                                     if ((deku_collection.count_documents( { "item" : { "$eq": item_bought } } )) == 0):
-                                        log_collection.insert_one( { "log" : "Queried database for existing STORE ITEM during TRANSACTION!", "when": datetime.now() } )
+                                        log_collection.insert_one( { "log" : "Queried database for non-existing STORE ITEM during TRANSACTION!", "when": datetime.now() } )
                                         print("The item you searched for does not exist!")
                                         continue
-                                    log_collection.insert_one( { "log" : f"QEURIEd for current amount in wallet during TRANSACTION!", "when": datetime.now() } )
                                     item_to_buy = deku_collection.distinct("cost", { "item" : item_bought } )
                                     log_collection.insert_one( { "log" : f"QEURIEd for item cost during TRANSACTION!", "when": datetime.now() } )
                                     # print(item_to_buy[0])
@@ -279,14 +295,34 @@ def main():
                                         wallet_after_sale = customer_collection.distinct("wallet", { "user" : customer_username.lower() } )
                                         log_collection.insert_one( { "log" : f"QUERIEd customer wallet AFTER TRANSACTION!", "when" : datetime.now() } )
                                         print(f"Your wallet now has {wallet_after_sale[0]} rupees!")
-                                        print("\n")
                                         log_collection.insert_one( { "log" : f"UPDATEd customer wallet to reflect his/her purchase!", "when": datetime.now() } )
                                         inventory_collection.insert_one( { "item" : item_bought, "sell_price" : item_to_buy[0] - int(item_to_buy[0] * .2) } )
                                         log_collection.insert_one( { "log" : f"UPDATEd customer inventory with item and sale value!", "when": datetime.now() } )
                                         deku_collection.delete_one( { "item" : item_bought } )
-                                        log_collection.insert_one( { "log" : f"DELETEd purchased item from the inventory of items!", "when": datetime.now() } )
+                                        log_collection.insert_one( { "log" : f"DELETEd purchased item from the Deku Store's items!", "when": datetime.now() } )
                                 case('s'):
-                                    pass
+                                    print("These are the items you have in your inventory to sell to the Deku Store!")
+                                    for item in entire_inventory:
+                                        print(item["item"], " | sale value: " + str(item["sell_price"]))
+                                    current_sell_wallet = customer_collection.distinct("wallet", { "user" : customer_username.lower() } )
+                                    print(f"Your wallet has {current_sell_wallet[0]} rupees!")
+                                    log_collection.insert_one( { "log" : f"QEURIEd for current amount in wallet during SALE!", "when": datetime.now() } )
+                                    item_selling = input("What item would you like to sell? ")
+                                    if ((inventory_collection.count_documents( { "item" : { "$eq": item_selling } } )) == 0):
+                                        log_collection.insert_one( { "log" : "Queried database for non-existing STORE ITEM during SALE!", "when": datetime.now() } )
+                                        print("The item you searched for does not exist!")
+                                        continue
+                                    item_to_sell = inventory_collection.distinct("sell_price", { "item" : item_selling } )
+                                    log_collection.insert_one( { "log" : f"QEURIEd for item cost during SALE!", "when": datetime.now() } )
+                                    customer_collection.update_one( { "user" : customer_username.lower() }, { "$inc" : { "wallet" : item_to_sell[0] } } )
+                                    wallet_after_sale = customer_collection.distinct("wallet", { "user" : customer_username.lower() } )
+                                    log_collection.insert_one( { "log" : f"QUERIEd customer wallet AFTER SALE!", "when" : datetime.now() } )
+                                    print(f"Your wallet now has {wallet_after_sale[0]} rupees!")
+                                    log_collection.insert_one( { "log" : f"UPDATEd customer wallet to reflect his/her sale of item!", "when": datetime.now() } )
+                                    deku_collection.insert_one( { "item" : item_selling, "cost" : item_to_sell[0] + int(item_to_sell[0] * .3) } )
+                                    log_collection.insert_one( { "log" : f"UPDATEd Deku Store with item and sale value!", "when": datetime.now() } )
+                                    inventory_collection.delete_one( { "item" : item_selling } )
+                                    log_collection.insert_one( { "log" : f"DELETEd purchased item from the inventory of items!", "when": datetime.now() } )
 
                                 case('e'):
                                     break
